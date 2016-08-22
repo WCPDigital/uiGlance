8274a6e54617651ab35beea8bae94900a3d684d4
PatP77 <patp77@wcpdigital.com.au>
2016-08-23 08:55:16 +1000

    Comment changes

--- src/Standalone/uiglance.min.js
+++ src/Standalone/uiglance.min.js
@@ -2,9 +2,11 @@
  * uiGlance v1.0.0
  * http://uiglance.com
  *
- * Copyright WCP Digital and Patrick Purcell
- * Released under the MIT license
- * http://opensource.org/licenses/MIT
+ * @author Patrick Purcell
+ * @copyright Copyright (c) 2016 WCP Digital
+ * @license http://opensource.org/licenses/MIT
+ * @link http://www.wcpdigital.com.au
+ * @link http://patrickpurcell.bio
  *
  * Date: 2016-07-25
  */
